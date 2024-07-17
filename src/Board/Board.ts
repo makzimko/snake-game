6aba2746323dd383cc5fb097325e6d5ce42c09f2
Maksym Tkachuk <maksym.tkachuk@atlaslive.tech>
2024-07-17 20:32:49 +0300

    Added flat board implementation

---
+++ src/Board/Board.ts
@@ -0,0 +1,198 @@
+import Snake, {Segment} from "../Snake";
+import { Direction, TurnDirection } from "../types.ts";
+import {getDirectionTurn, getNewDirection, oppositeDirections} from "../direction.ts";
+import {CollisionError} from "../errors.ts";
+
+export enum Cell {
+  FREE,
+  SNAKE,
+  FOOD
+}
+
+export enum CrashCause {
+  COLLISION,
+  SIDE
+}
+
+type BoardSettings = {
+  width: number,
+  height: number,
+  speed: number,
+  startX: number,
+  startY: number,
+  startDirection: Direction,
+  snakeLength?: number,
+}
+
+const getSegmentDirection = (segment: Segment) => {
+  switch (segment) {
+    case Segment.TURN_LEFT:
+    case Segment.HEAD_RIGHT:
+      return TurnDirection.LEFT;
+    case Segment.TURN_RIGHT:
+    case Segment.HEAD_LEFT:
+      return TurnDirection.RIGHT;
+  }
+}
+
+class Board {
+  readonly #settings: BoardSettings = {
+    width: 10,
+    height: 20,
+    speed: 500,
+    startX: 0,
+    startY: 0,
+    startDirection: Direction.UP,
+    snakeLength: 3,
+  }
+  readonly #snake: Snake;
+
+  #snakeHead: {
+    x: number,
+    y: number,
+  };
+  #foodPosition: {
+    x: number,
+    y: number
+  } = {
+    x: 0,
+    y: 0,
+  };
+  #snakeDirection: Direction;
+  #playInterval?: ReturnType<typeof setInterval>;
+  readonly #headUpdate: Record<Direction, [number, number]> = {
+    [Direction.UP]: [0, 1],
+    [Direction.DOWN]: [0, -1],
+    [Direction.LEFT]: [-1, 0],
+    [Direction.RIGHT]: [1, 0],
+  };
+
+  onRender?: (board: Cell[][]) => void;
+  onCrash?: (cause: CrashCause) => void;
+
+  constructor(props: Partial<BoardSettings> = {}) {
+    this.#settings = {
+      ...this.#settings,
+      ...props,
+    };
+    this.#snake = new Snake({ length: this.#settings.snakeLength });
+
+    const { startX, startY, startDirection } = this.#settings;
+
+    this.#snakeHead = {
+      x: startX,
+      y: startY
+    };
+    this.#snakeDirection = startDirection;
+    this.generateFood();
+  }
+
+  play() {
+    this.updateBoard();
+    this.#playInterval = setInterval(this.makeMove.bind(this), this.#settings.speed)
+  }
+
+  pause() {
+    clearInterval(this.#playInterval);
+  }
+
+  private finish(cause: CrashCause) {
+    this.pause();
+    if (this.onCrash) {
+      this.onCrash(cause);
+    }
+  }
+
+  turn(direction: Direction) {
+    const turn = getDirectionTurn(this.#snakeDirection, direction);
+
+    if (turn === undefined) {
+      return false;
+    }
+
+    this.#snakeDirection = direction;
+    this.#snake.turn(turn);
+  }
+
+  private generateFood() {
+    this.#foodPosition.x = Math.floor(Math.random() * this.#settings.width);
+    this.#foodPosition.y = Math.floor(Math.random() * this.#settings.height);
+  }
+
+  private getEmptyBoard() {
+    const { width, height } = this.#settings;
+    return Array.from({ length: width }, () => Array(height).fill(Cell.FREE));
+  }
+
+  private makeMove() {
+    const [xDiff, yDiff] = this.#headUpdate[this.#snakeDirection];
+
+
+    try {
+      this.updateHeadPosition(xDiff, yDiff);
+
+      const feed = this.#snakeHead.x === this.#foodPosition.x && this.#snakeHead.y === this.#foodPosition.y;
+      if (feed) {
+        this.generateFood();
+      }
+      this.#snake.move(feed ? 1 : 0);
+      this.updateBoard();
+    } catch (e) {
+      if (e instanceof CollisionError) {
+        this.finish(CrashCause.COLLISION);
+      } else {
+        this.finish(CrashCause.SIDE);
+      }
+    }
+  }
+
+  private updateHeadPosition (xDiff: number, yDiff: number) {
+    const { x, y } = this.#snakeHead;
+    const newX = x + xDiff;
+    const newY = y + yDiff;
+
+    if (newX < 0 || newX >= this.#settings.width  || newY < 0 || newY >= this.#settings.height) {
+      throw Error('Side crash position');
+    }
+
+    this.#snakeHead = {
+      x: x + xDiff,
+      y: y + yDiff
+    };
+  }
+
+  private updateBoard() {
+    const board = this.getEmptyBoard();
+
+    board[this.#foodPosition.x][this.#foodPosition.y] = Cell.FOOD;
+
+    let { x, y } = this.#snakeHead;
+    let direction = oppositeDirections[this.#snakeDirection];
+
+    for (let i = 0; i < this.#snake.body.length; i++) {
+      board[x][y] = Cell.SNAKE;
+
+      const segment = this.#snake.body[i];
+      const turn = getSegmentDirection(segment);
+      const newDirection = turn !== undefined ? getNewDirection(direction, turn): direction;
+
+      let newX = x, newY = y;
+      if ([Direction.LEFT, Direction.RIGHT].includes(newDirection)) {
+        newX += newDirection === Direction.LEFT ? -1 : 1;
+      }
+      if ([Direction.UP, Direction.DOWN].includes(newDirection)) {
+        newY += newDirection === Direction.UP ? 1 : -1;
+      }
+
+      x = newX;
+      y = newY;
+      direction = newDirection;
+    }
+
+    if (this.onRender) {
+      this.onRender(board);
+    }
+  }
+}
+
+export default Board;
--- src/direction.ts
+++ src/direction.ts
@@ -1,7 +1,30 @@
 import {Direction, TurnDirection} from "./types.ts";
 
 const DIRECTION_VECTORS = [Direction.UP, Direction.LEFT, Direction.DOWN, Direction.RIGHT];
+
 export const getNewDirection = (direction: Direction, turn: TurnDirection) => {
     const index = DIRECTION_VECTORS.indexOf(direction) + (turn === TurnDirection.LEFT ? 1 : -1);
     return DIRECTION_VECTORS[index] ?? DIRECTION_VECTORS[index - 4] ?? DIRECTION_VECTORS[index + 4]
+}
+
+export const getDirectionTurn = (direction: Direction, newDirection: Direction): TurnDirection | void => {
+    if (isDirectionAdjacent(direction, newDirection)) {
+        const distance = DIRECTION_VECTORS.indexOf(newDirection) - DIRECTION_VECTORS.indexOf(direction);
+        if ((distance > 0 && distance < 3) || distance < -2) {
+            return TurnDirection.LEFT;
+        }
+        return TurnDirection.RIGHT;
+    }
+}
+
+export const isDirectionAdjacent = (direction: Direction, newDirection: Direction) => {
+    const distance = Math.abs(DIRECTION_VECTORS.indexOf(direction) - DIRECTION_VECTORS.indexOf(newDirection));
+    return [1, 3].includes(distance);
+}
+
+export const oppositeDirections: Record<Direction, Direction> = {
+    [Direction.UP]: Direction.DOWN,
+    [Direction.DOWN]: Direction.UP,
+    [Direction.LEFT]: Direction.RIGHT,
+    [Direction.RIGHT]: Direction.LEFT,
 }
